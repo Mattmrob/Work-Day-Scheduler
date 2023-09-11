58ff497c5e136ea2bb5aedbbdff4f9c8e297706a
Mysmelf <matthewr9937@gmail.com>
2023-09-11 13:09:22 -0700

    Added function which allows loading of stored textarea content to load onto the page when page is refresh or reopened. Content loaded from previously implemented save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,13 @@ function linearTime() {
 
 }
 
+// Initialization function / load previously stored values
+
+function loadStorage() {
+  let hour9Content = JSON.parse(localStorage.getItem("hour-9"));
+  $('#hour-9').children('textarea').val(hour9Content);
+}
+
 // Function Execution and DOM traversal
 
 $(function () {
@@ -39,7 +46,9 @@ $(function () {
   dateArea.text(today.format('dddd, MMMM DD'));
 
   linearTime();
+  loadStorage();
 
+// Save your input to local storage!
   saveBtn.on('click', function(event) {
     event.preventDefault();
 
@@ -51,30 +60,5 @@ $(function () {
 
     localStorage.setItem(targetId, JSON.stringify(textValue));
   })
-
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-
-  // TODO: Add code to display the current date in the header of the page.
 
 });
