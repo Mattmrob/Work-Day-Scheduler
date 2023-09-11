a09927337e062450f0fe16a986278288b8b45d5d
Mysmelf <matthewr9937@gmail.com>
2023-09-11 11:45:17 -0700

    Successfully added jquery function that adds past class to divs that time has aleady passed

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,35 +5,35 @@
 const dateArea = $('#currentDay');
 const today = dayjs();
 
-let dataValue = $('.time-block').data("value");
-let hour = $(dayjs().$H);
-
-
+let hour = today.format("H");
+let hourInt = parseInt(hour);
 
 
 function linearTime() {
 
-  $('.time-block').each(function(){
-    if ($(this).data("value") < hour) {
-      $('div').addClass('past');
+  $('div.time-block').each(function(){
+    if ($(this).data("value") < hourInt) {
+    $(this).addClass("past");
+    console.log('it worked');
     }
     console.log($(this).data("value"))
-    console.log(hour);
+    console.log(this)
+    console.log(hourInt);
   })
 
 }
 
-
+console.log(hourInt);
 
 
 $(function () {
 
   dateArea.text(today.format('dddd, MMMM DD'));
 
-  console.log(dayjs());
-  console.log(hour);
+  // console.log(dayjs());
+  // console.log(hour);
 
-  console.log($('#hour-9').data('value'));
+  // console.log($('#hour-9').data('value'));
 
   linearTime();
 
